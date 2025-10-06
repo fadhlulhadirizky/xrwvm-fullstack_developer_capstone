bc4ad82d244b5463cf07d6e733e93e8b7bd91313
fadhlulhadirizky <fadhlulhadirizky@gmail.com>
2025-10-06 09:43:07 -0400

    Initial Django project push

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,15 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+    founded_date = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +32,35 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    dealer_id = models.IntegerField()  # Refers to a dealer created in Cloudant database
+    name = models.CharField(max_length=100)
+    
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
+    
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    
+    # Other fields as needed
+    color = models.CharField(max_length=50, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"  # Return car make, model name and year
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,55 @@
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    try:
+        print("Starting populate...")
+        
+        car_make_data = [
+            {"name":"NISSAN", "description":"Great cars. Japanese technology"},
+            {"name":"Mercedes", "description":"Great cars. German technology"},
+            {"name":"Audi", "description":"Great cars. German technology"},
+            {"name":"Kia", "description":"Great cars. Korean technology"},
+            {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        ]
+
+        car_make_instances = []
+        for data in car_make_data:
+            car_make = CarMake.objects.create(name=data['name'], description=data['description'])
+            car_make_instances.append(car_make)
+            print(f"Created CarMake: {car_make.name}")
+
+        # Create CarModel instances with the corresponding CarMake instances
+        car_model_data = [
+          {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
+          {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
+          {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
+          {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
+          {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
+          {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
+          {"name":"A4", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
+          {"name":"A5", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
+          {"name":"A6", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
+          {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
+          {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
+          {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":car_make_instances[3]},
+          {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
+          {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
+          {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4]},
+        ]
+
+        for data in car_model_data:
+            car_model = CarModel.objects.create(
+                name=data['name'], 
+                car_make=data['car_make'], 
+                type=data['type'], 
+                year=data['year'],
+                dealer_id=1  # Add a default dealer_id since it's required
+            )
+            print(f"Created CarModel: {car_model.car_make.name} {car_model.name}")
+
+        print("Populate completed successfully!")
+        
+    except Exception as e:
+        print(f"Error during populate: {e}")
+        import traceback
+        traceback.print_exc()
